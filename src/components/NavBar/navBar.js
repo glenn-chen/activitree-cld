1f62c55d401c8738a38425ce9cc34eb243b7f7f1
Glenn Chen <glenn.jun.chen@gmail.com>
2022-01-25 18:22:39 -0800

    added activitree logo and title to navbar

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -4,47 +4,54 @@ import './navBar.css';
 export default class NavBar extends Component {    
     render() {
         return (
-            <div className="navBarRow">
-                <a
-                    className="navBarLink"
-                    href="/welcome"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Welcome
-                </a>
-                <a
-                    className="navBarLink"
-                    href="/schedule"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Schedule
-                </a>
-                <a
-                    className="navBarLink"
-                    href="/shoutouts"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Shoutouts
-                </a>
-                <a
-                    className="navBarLink"
-                    href="/about-us"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    About Us
-                </a>
-                <a
-                    className="navBarLink"
-                    href="/sponsors"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Sponsors
-                </a>
+            <div>
+                <img className="navBarImage"
+                src="../../../ActivitreeLogo.png"
+                alt="ActivitreeLogopng"></img>
+                <h2 className="navBarTitle">College Learn-About Day</h2>
+                <div className="navBarRow">
+                
+                    <a
+                        className="navBarLink"
+                        href="/welcome"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Welcome
+                    </a>
+                    <a
+                        className="navBarLink"
+                        href="/schedule"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Schedule
+                    </a>
+                    <a
+                        className="navBarLink"
+                        href="/shoutouts"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Shoutouts
+                    </a>
+                    <a
+                        className="navBarLink"
+                        href="/about-us"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        About Us
+                    </a>
+                    <a
+                        className="navBarLink"
+                        href="/sponsors"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Sponsors
+                    </a>
+                </div>
             </div>
         );
     }
