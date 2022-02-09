eacd66fa1c49102e77a7446bb279c0c3f92289ae
Glenn Chen <glenn.jun.chen@gmail.com>
2022-02-09 00:31:18 -0800

    cleaned up text and button color classes

--- src/components/ShortInfoBlock/shortInfoBlock.js
+++ src/components/ShortInfoBlock/shortInfoBlock.js
@@ -2,32 +2,27 @@ import React, {Component} from 'react';
 import './shortInfoBlock.css';
 
 export default class ShortInfoBlock extends Component {
-    constructor(props) {
-        super(props);
-    }
 
     render() {
         let containerVersion = "container-" + this.props.version;
 
         // Default text/buttons are blue
-        let h1Version = "h1-blue";
-        let h2Version = "h2-blue";
-        let buttonVersion = "button-blue";
+        let textVersion = "blue";
+        let buttonVersion = "blue";
 
         // White text/buttons on blue background
-        if (this.props.version == "blue") {
-            h1Version = "h1-white";
-            h2Version = "h2-white";
-            buttonVersion = "button-white";
+        if (this.props.version === "blue") {
+            textVersion = "white";
+            buttonVersion = "white";
         }
         
         return (
             <div class={containerVersion}>
                 <br />
-                <h1 class={h1Version}>
+                <h1 class={textVersion}>
                     {this.props.header}
                 </h1>
-                <h2 class={h2Version}>{this.props.description}</h2>
+                <h2 class={textVersion}>{this.props.description}</h2>
                 <div class="center">
                     <a class="button-link" href={this.props.link}>
                     <button class={buttonVersion}>
