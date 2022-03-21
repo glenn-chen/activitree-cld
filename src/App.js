8420d3e0171945cd8b924590e3d4f3c6bbab0ec9
Arnav Rastogi <arnavrastogi2002@gmail.com>
2022-03-21 16:53:10 -0700

    hashrouter

--- src/App.js
+++ src/App.js
@@ -8,19 +8,23 @@ import Shoutouts from './components/Shoutouts/shoutouts';
 import Sponsors from './components/Sponsors/sponsors';
 import Footer from './components/Footer/footer';
 import About from './components/AboutUs/aboutUs';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+
+
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/activitree-cld" element={<React.Fragment><NavBar/><Welcome/><Footer/></React.Fragment>}/>
-        <Route path="/activitree-cld/Welcome" element={<React.Fragment><NavBar/><Welcome/><Footer/></React.Fragment>}/>
+        <Route path="/activitree-cld/welcome" element={<React.Fragment><NavBar/><Welcome/><Footer/></React.Fragment>}/>
         <Route path="/activitree-cld/schedule" element={<React.Fragment><NavBar/><Schedule/><Footer/></React.Fragment>}/>
         <Route path="/activitree-cld/shoutouts" element={<React.Fragment><NavBar/><Shoutouts/><Footer/></React.Fragment>}/>
         <Route path="/activitree-cld/about" element={<React.Fragment><NavBar/><About/><Footer/></React.Fragment>}/>
         <Route path="/activitree-cld/sponsors" element={<React.Fragment><NavBar/><Sponsors/><Footer/></React.Fragment>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
+
+//<Route path="/activitree-cld" element={<React.Fragment><NavBar/><Welcome/><Footer/></React.Fragment>}/>
--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -13,10 +13,10 @@ export default class NavBar extends Component {
                 <h2 className="navBarTitle">College Learn-About Day!</h2>
                 <hr className="navBarLine"></hr>
                 <div className="navBarRow">
-                
+
                     <a
                         className="navBarLink"
-                        href="/activitree-cld/welcome"
+                        href="/#/activitree-cld/welcome"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
@@ -24,7 +24,7 @@ export default class NavBar extends Component {
                     </a>
                     <a
                         className="navBarLink"
-                        href="/activitree-cld/schedule"
+                        href="/#/activitree-cld/schedule"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
@@ -32,7 +32,7 @@ export default class NavBar extends Component {
                     </a>
                     <a
                         className="navBarLink"
-                        href="/activitree-cld/shoutouts"
+                        href="/#/activitree-cld/shoutouts"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
@@ -40,7 +40,7 @@ export default class NavBar extends Component {
                     </a>
                     <a
                         className="navBarLink"
-                        href="/activitree-cld/about"
+                        href="/#/activitree-cld/about"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
@@ -48,7 +48,7 @@ export default class NavBar extends Component {
                     </a>
                     <a
                         className="navBarLink"
-                        href="/activitree-cld/sponsors"
+                        href="/#/activitree-cld/sponsors"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
