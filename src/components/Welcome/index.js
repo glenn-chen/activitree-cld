4787df729699fc5042dc7e044a238768176333a8
Glenn Chen <glenn.jun.chen@gmail.com>
2022-03-31 14:44:18 -0700

    added sign up form

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -16,23 +16,29 @@ export default class Welcome extends Component {
                     <div class="centered">
                         <ShortInfoBlock
                             header="Welcome to College Learn-About Day!"
-                            description="April 2, 2022"
+                            description="May 7, 2022"
                             version="clear"
                             textSize="big"
-                            buttonText="GET ZOOM LINK"
-                            link="https://ucdavis.zoom.us/j/92110391733"
+                            buttonText="SIGN UP FORM"
+                            link="https://docs.google.com/forms/d/1YcnC-avcZVBX_OakJNLsc4Vha6NZ6NHWx4HPaq4-BJE/edit "
                         />
                     </div>
                 </div>
                 <ShortInfoBlock
-                    header="Join the College Learn-About Day Discord!"
+                    header="Join us online on Zoom!"
                     version="blue"
+                    buttonText="GET THE LINK"
+                    link="https://ucdavis.zoom.us/j/92110391733"
+                />
+                <ShortInfoBlock
+                    header="Join the College Learn-About Day Discord!"
+                    version="white"
                     buttonText="JOIN THE DISCORD!"
                     link="https://discord.gg/CS2J3WJBzv"
                 />
                 <ShortInfoBlock
                     header="Draw your favorite food and sign your name on the Whiteboard!"
-                    version="white"
+                    version="blue"
                     buttonText="Take me to the whiteboard!"
                     link="https://jamboard.google.com/d/1q8xJl-1bV0yzqplhqZnN1RVOC82fUyOPUpJ4xR1CzGI/edit?usp=sharing"
                 />
